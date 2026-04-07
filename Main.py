48df3780fad7fdee1b938253943b68dd457b82d8
KadmelHaller <c.espinoza33@gmail.com>
2026-04-07 15:44:57 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -87,7 +87,7 @@ def procesar_ia(datos_crudos):
         FORMATO ESTRICTO POR NOTICIA:
         *TITULAR EXACTO EN MAYÚSCULAS*
         MEDIO EN MAYÚSCULAS
-        Copia los primeros dos párrafos del cuerpo de la nota y júntalos en un solo párrafo, no tomes en cuenta el lead o entradilla. NO cambies ni acortes nombres ni cargos, sin excepción.
+        Resumen palabra por palabra de los primeros 2 a 3 párrafos, combinados en un solo parrafo, sin cambios en el texto para que sean entre 4 a 6 líneas. NO cambies ni acortes nombres ni cargos, sin excepción.
         Enlace (La URL correspondiente SIN ETIQUETA)
         
         """
